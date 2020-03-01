e0e1371f78be5df0a78948e142c20211d60587a4
Mike Holm <coldpour@gmail.com>
2020-03-01 11:02:50 -0700

    style songs

--- src/components/Songs.js
+++ src/components/Songs.js
@@ -3,13 +3,18 @@ import React from "react";
 export default ({ songs }) => (
   <div>
     <h1>Songs ({songs.length})</h1>
-    <table>
+    <table
+      style={{
+        margin: "auto"
+      }}
+    >
       <tbody>
         {songs.map(({ title, artist }) => {
           return (
             <tr key={title + artist}>
-              <td>{title}</td>
-              <td>{artist}</td>
+              <td style={{ textAlign: "right" }}>{title}</td>
+              <td style={{ padding: "0 .5em" }}>-</td>
+              <td style={{ textAlign: "left" }}>{artist}</td>
             </tr>
           );
         })}
