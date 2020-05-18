a2bfe86bd845a1dd5721c5fe7b0bcf98ecc8ca69
Mike Holm <coldpour@gmail.com>
2020-05-17 19:01:51 -0600

    add emotion for css classes

--- src/components/Follow.js
+++ src/components/Follow.js
@@ -1,4 +1,6 @@
+/** @jsx jsx */
 import React from "react";
+import { css, jsx } from "@emotion/core";
 //
 import Social from "components/Social";
 import SubscribeForm from "components/SubscribeForm";
@@ -7,11 +9,11 @@ import Motto from "components/Motto";
 export default () => (
   <div style={{ padding: "3em 1em", flex: 1 }}>
     <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between"
-      }}
+      css={css`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+      `}
     >
       <div style={{ fontSize: "2em" }}>follow:</div>
       <Social />
--- static.config.js
+++ static.config.js
@@ -49,6 +49,7 @@ export default {
         location: path.resolve("./src/pages")
       }
     ],
+    require.resolve("react-static-plugin-emotion"),
     require.resolve("react-static-plugin-reach-router"),
     require.resolve("react-static-plugin-sitemap")
   ]
