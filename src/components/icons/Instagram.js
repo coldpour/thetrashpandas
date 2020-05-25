af1b03c955ce369515a25482594f7ee3be3aea04
Mike Holm <coldpour@gmail.com>
2020-05-24 21:21:40 -0600

    bolden svgs

--- src/components/icons/Instagram.js
+++ src/components/icons/Instagram.js
@@ -3,10 +3,10 @@ import React from "react";
 import Svg from "components/Svg";
 
 const Instagram = props => (
-  <Svg viewBox="-5 -5 55 55" stroke="white" strokeWidth="2" {...props}>
-    <rect x="1" y="1" width="48" height="48" rx="10" />
+  <Svg viewBox="-5 -5 55 55" stroke="white" strokeWidth="4" {...props}>
+    <rect x="2" y="2" width="45" height="45" rx="13" />
     <circle cx="25" cy="25" r="10" />
-    <circle cx="40" cy="10" r="3" />
+    <circle cx="37" cy="13" r="3" stroke="none" fill="white" />
   </Svg>
 );
 
