76d69e6efb9e1c121c099d94edf81f4d92c4b134
Mike Holm <coldpour@gmail.com>
2020-07-13 14:51:48 -0600

    update motto

--- src/components/Motto.js
+++ src/components/Motto.js
@@ -4,29 +4,15 @@ import { css, jsx } from "@emotion/core";
 const Motto = (props) => (
   <div
     css={css`
-      padding: 2em;
+      background: white;
+      color: black;
+      font-size: 3em;
+      padding: 1em 0;
     `}
   >
-    <div
-      css={css`
-        font-size: 3em;
-        font-wieght: 900;
-      `}
-      {...props}
-    >
-      <div>pop</div>
-      <div>rock</div>
-      <div>mosh</div>
-      <div>disco</div>
-    </div>
-    <div
-      css={css`
-        margin-top: 1em;
-        font-size: 1.25em;
-      `}
-    >
-      ...you're gonna dance
-    </div>
+    <div>you're</div>
+    <div>gonna</div>
+    <div>dance</div>
   </div>
 );
 
