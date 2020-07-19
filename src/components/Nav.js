1a1571895a6cf5cbdb2691da060f27145abdad4e
Mike Holm <coldpour@gmail.com>
2020-07-19 00:03:58 -0600

    flex the subnav under the name only

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,8 +1,10 @@
 /** @jsx jsx */
 import { css, jsx } from "@emotion/core";
 
+import Logo from "components/Logo";
+import NavLink from "components/NavLink";
+import Name from "components/Name";
 import MerchLink from "components/links/MerchLink";
-import HomeLink from "components/links/HomeLink";
 import BookLink from "components/links/BookLink";
 
 const SubNav = (props) => (
@@ -10,6 +12,15 @@ const SubNav = (props) => (
     css={css`
       display: flex;
       justify-content: space-evenly;
+    `}
+    {...props}
+  />
+);
+
+const NavLogo = (props) => (
+  <Logo
+    css={css`
+      height: 60px;
     `}
     {...props}
   />
@@ -25,11 +36,46 @@ const Nav = (props) => {
       `}
       {...props}
     >
-      <HomeLink />
-      <SubNav>
-        <MerchLink />
-        <BookLink />
-      </SubNav>
+      <div
+        css={css`
+          padding: 0.5em 0 1.5em;
+          display: flex;
+          align-items: center;
+        `}
+        {...props}
+      >
+        <NavLink to="/">
+          <NavLogo
+            css={css`
+              flex: 0 0 100px;
+            `}
+          />
+        </NavLink>
+
+        <div
+          css={css`
+            position: relative;
+          `}
+        >
+          <NavLink to="/">
+            <Name
+              css={css`
+                font-size: 14px;
+              `}
+            />
+          </NavLink>
+          <SubNav
+            css={css`
+              position: absolute;
+              width: 100%;
+              top: 3.5em;
+            `}
+          >
+            <MerchLink />
+            <BookLink />
+          </SubNav>
+        </div>
+      </div>{" "}
     </nav>
   );
 };
